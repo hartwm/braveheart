3dfbcc7527ffe427a7024e171d30ca964a38b29d
Will Hart <whart@davisadagency.com>
2019-03-05 14:25:50 -0500

    cleanup

--- inc/setup.php
+++ inc/setup.php
@@ -5,18 +5,18 @@
  * @package understrap
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
+if (!defined('ABSPATH')) {
 	exit; // Exit if accessed directly.
 }
 
 // Set the content width based on the theme's design and stylesheet.
-if ( ! isset( $content_width ) ) {
+if (!isset($content_width)) {
 	$content_width = 640; /* pixels */
 }
 
-add_action( 'after_setup_theme', 'understrap_setup' );
+add_action('after_setup_theme', 'understrap_setup');
 
-if ( ! function_exists ( 'understrap_setup' ) ) {
+if (!function_exists('understrap_setup')) {
 	/**
 	 * Sets up theme defaults and registers support for various WordPress features.
 	 *
@@ -24,17 +24,18 @@ if ( ! function_exists ( 'understrap_setup' ) ) {
 	 * runs before the init hook. The init hook is too late for some features, such
 	 * as indicating support for post thumbnails.
 	 */
-	function understrap_setup() {
+	function understrap_setup()
+	{
 		/*
 		 * Make theme available for translation.
 		 * Translations can be filed in the /languages/ directory.
 		 * If you're building a theme based on understrap, use a find and replace
 		 * to change 'understrap' to the name of your theme in all the template files
 		 */
-		load_theme_textdomain( 'understrap', get_template_directory() . '/languages' );
+		load_theme_textdomain('understrap', get_template_directory() . '/languages');
 
 		// Add default posts and comments RSS feed links to head.
-		add_theme_support( 'automatic-feed-links' );
+		add_theme_support('automatic-feed-links');
 
 		/*
 		 * Let WordPress manage the document title.
@@ -42,66 +43,60 @@ if ( ! function_exists ( 'understrap_setup' ) ) {
 		 * hard-coded <title> tag in the document head, and expect WordPress to
 		 * provide it for us.
 		 */
-		add_theme_support( 'title-tag' );
+		add_theme_support('title-tag');
 
 		// This theme uses wp_nav_menu() in one location.
-		register_nav_menus( array(
-			'primary' => __( 'Primary Menu', 'understrap' ),
-		) );
+		register_nav_menus(array(
+			'primary' => __('Primary Menu', 'understrap'),
+		));
 
 		/*
 		 * Switch default core markup for search form, comment form, and comments
 		 * to output valid HTML5.
 		 */
-		add_theme_support( 'html5', array(
+		add_theme_support('html5', array(
 			'search-form',
 			'comment-form',
 			'comment-list',
 			'gallery',
 			'caption',
-		) );
+		));
 
 		/*
 		 * Adding Thumbnail basic support
 		 */
-		add_theme_support( 'post-thumbnails' );
+		add_theme_support('post-thumbnails');
 
 		/*
 		 * Adding support for Widget edit icons in customizer
 		 */
-		add_theme_support( 'customize-selective-refresh-widgets' );
+		add_theme_support('customize-selective-refresh-widgets');
 
 		/*
 		 * Enable support for Post Formats.
 		 * See http://codex.wordpress.org/Post_Formats
 		 */
-		add_theme_support( 'post-formats', array(
+		add_theme_support('post-formats', array(
 			'aside',
 			'image',
 			'video',
 			'quote',
 			'link',
-		) );
+		));
 
-		// Set up the WordPress core custom background feature.
-		add_theme_support( 'custom-background', apply_filters( 'understrap_custom_background_args', array(
-			'default-color' => 'ffffff',
-			'default-image' => '',
-		) ) );
 
 		// Set up the WordPress Theme logo feature.
-		add_theme_support( 'custom-logo' );
+		add_theme_support('custom-logo');
 
 		// Check and setup theme default settings.
 		understrap_setup_theme_default_settings();
-
 	}
 }
 
 
-add_filter( 'excerpt_more', 'understrap_custom_excerpt_more' );
+add_filter('excerpt_more', 'understrap_custom_excerpt_more');
 
-if ( ! function_exists( 'understrap_custom_excerpt_more' ) ) {
+if (!function_exists('understrap_custom_excerpt_more')) {
 	/**
 	 * Removes the ... from the excerpt read more link
 	 *
@@ -109,17 +104,18 @@ if ( ! function_exists( 'understrap_custom_excerpt_more' ) ) {
 	 *
 	 * @return string
 	 */
-	function understrap_custom_excerpt_more( $more ) {
-		if ( ! is_admin() ) {
+	function understrap_custom_excerpt_more($more)
+	{
+		if (!is_admin()) {
 			$more = '';
 		}
 		return $more;
 	}
 }
 
-add_filter( 'wp_trim_excerpt', 'understrap_all_excerpts_get_more_link' );
+add_filter('wp_trim_excerpt', 'understrap_all_excerpts_get_more_link');
 
-if ( ! function_exists( 'understrap_all_excerpts_get_more_link' ) ) {
+if (!function_exists('understrap_all_excerpts_get_more_link')) {
 	/**
 	 * Adds a custom read more link to all excerpts, manually or automatically generated
 	 *
@@ -127,10 +123,13 @@ if ( ! function_exists( 'understrap_all_excerpts_get_more_link' ) ) {
 	 *
 	 * @return string
 	 */
-	function understrap_all_excerpts_get_more_link( $post_excerpt ) {
-		if ( ! is_admin() ) {
-			$post_excerpt = $post_excerpt . ' [...]<p><a class="btn btn-secondary understrap-read-more-link" href="' . esc_url( get_permalink( get_the_ID() ) ) . '">' . __( 'Read More...',
-			'understrap' ) . '</a></p>';
+	function understrap_all_excerpts_get_more_link($post_excerpt)
+	{
+		if (!is_admin()) {
+			$post_excerpt = $post_excerpt . ' [...]<p><a class="btn btn-secondary understrap-read-more-link" href="' . esc_url(get_permalink(get_the_ID())) . '">' . __(
+				'Read More...',
+				'understrap'
+			) . '</a></p>';
 		}
 		return $post_excerpt;
 	}
